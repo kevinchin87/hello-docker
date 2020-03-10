bb965e57a7d731ab959400272da52a3095f195fe
Kevin Chin <kevin.chin.87@gmail.com>
2020-03-10 23:22:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello again world!\n');
+  res.send('Hello again from google cloud world!\n');
 });
 
 app.listen(PORT, HOST);
